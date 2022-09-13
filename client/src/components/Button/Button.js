d05ab394ac15170477e77c2fbc174050c89e7d47
Ilka <nemmilka@gmail.com>
2022-09-13 20:15:37 +0200

    feat(client/src/components/Button): text prop

---
+++ client/src/components/Button/Button.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './Button.css';
+
+const Button = ({text}) => {
+  return <div className='button'>{text}</div>;
+};
+
+export default Button;
