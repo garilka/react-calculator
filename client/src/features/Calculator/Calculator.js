dd4856c7556b2469e59f02d28657d31af042b24d
Ilka <nemmilka@gmail.com>
2022-09-14 10:20:30 +0200

    feat(client/src/features/Calculator): import Display

--- client/src/features/Calculator/Calculator.js
+++ client/src/features/Calculator/Calculator.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import './Calculator.css';
 import Button from '../../components/Button/Button';
+import Display from '../../components/Display/Display';
 
 const Calculator = () => {
   return (
     <div className='wrapper'>
+      <Display/>
+      <div className='row'>
+        <Button text='C'/>
+        <Button text='MR'/>
+        <Button text='M+'/>
+        <Button text='/'/>
+      </div>
       <div className='row'>
         <Button text='7'/>
         <Button text='8'/>
-        <Button/>
-        <Button/>
+        <Button text='9'/>
+        <Button text='×'/>
       </div>
       <div className='row'>
-        <Button/>
-        <Button/>
-        <Button/>
-        <Button/>
+        <Button text='4'/>
+        <Button text='5'/>
+        <Button text='6'/>
+        <Button text='-'/>
       </div>
       <div className='row'>
-        <Button/>
-        <Button/>
-        <Button/>
-        <Button/>
+        <Button text='1'/>
+        <Button text='2'/>
+        <Button text='3'/>
+        <Button text='+'/>
       </div>
       <div className='row'>
-        <Button/>
-        <Button/>
-        <Button/>
-        <Button/>
+        <Button text='0'/>
+        <Button text='.'/>
+        <Button text='='/>
       </div>
     </div>
   );
