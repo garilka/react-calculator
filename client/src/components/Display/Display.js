7a3875d2ffae5049c642689bde0f61afad7e4da4
Ilka <nemmilka@gmail.com>
2022-09-14 10:19:54 +0200

    feat(client/src/components): create Display

---
+++ client/src/components/Display/Display.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Display.css';
+
+const Display = () => {
+  return (
+    <div className='display'>
+      <div className='result'>Hello</div>
+      <div className='equation'>Hi</div>
+    </div>
+  );
+};
+
+export default Display;
