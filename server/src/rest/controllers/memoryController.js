81a354dcf9d06018e6c05ca93028e88c080fca57
Ilka <nemmilka@gmail.com>
2022-09-15 20:42:18 +0200

    feat(server/src/controllers/memoryController): add fs.read & fs.write

--- server/src/rest/controllers/memoryController.js
+++ server/src/rest/controllers/memoryController.js
@@ -1,9 +1,23 @@
-const get = async (req, res) => {
+import fs from 'fs';
+import path from 'path';
 
+const filepath = path.resolve(
+    'G:/projects/calculator/server/src/memory', 'memory.txt');
+
+
+const get = async (req, res) => {
+  fs.promises.readFile(filepath, 'utf8')
+      .then((memory) => {
+        res.status(200).json({memory});
+      });
 };
 
 const put = (req, res) => {
-
+  fs.promises.writeFile(filepath, req.body.result.toString(), 'utf8')
+      .then(() => {
+        res.status(200).json({
+          message: `${req.body.result} succesfully saved in memory`});
+      });
 };
 
 export default {
