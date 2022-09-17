b58f4e5a81035bd47c7bbed38d7b6e659d980dbd
Ilka <nemmilka@gmail.com>
2022-09-17 12:17:22 +0200

    feat(calculator/client/src/components/Display): create memoryState prop

--- client/src/components/Display/Display.js
+++ client/src/components/Display/Display.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Display.css';
 
-const Display = ({result, equation}) => {
+const Display = ({memoryState, result, equation}) => {
   return (
     <div className='display'>
+      <div className='memoryState'>{memoryState}</div>
       <div className='result'>{result}</div>
       <div className='equation'>{equation}</div>
     </div>
