fde3877a365c8a08b53a0bbbf7db4658355a35a8
Ilka <nemmilka@gmail.com>
2022-09-17 17:27:44 +0200

    feat(calculator/client/src/services/calculatorValidation): add calculation & MR validation

--- client/src/services/calculatorValidation.js
+++ client/src/services/calculatorValidation.js
@@ -1,34 +1,58 @@
-const isValid = (arr, value) => {
-  if (arr.length === 0 && /([/|+|.|×])/.test(value)) {
-    return false;
-  }
-  if (arr[arr.length - 2] === '×' && arr[arr.length - 1] === '×' &&
-    /([/|×|\-|+|.])/.test(value)) {
-    return false;
-  }
-  if (arr[arr.length - 1] === '×' && /([/|\-|+|.])/.test(value)) {
-    return false;
-  }
-  if (/([/|\-|+|.])/.test(arr[arr.length - 1]) &&
-    /([/|\-|+|.|×])/.test(value)) {
-    return false;
-  }
-  if (value === '.') {
-    const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
-    return !lastNumber.includes('.') ? true : false;
-  }
-  if (value === '0' && arr.length > 0) {
-    const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
-    return lastNumber === '0' &&
-    !lastNumber.includes('.') ?
-     false : true;
-  }
-  if (/([1-9])/.test(value) && arr.length > 0) {
-    const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
-    return lastNumber.charAt(0) === '0' && !lastNumber.includes('.') ?
-     false : true;
-  }
-  return true;
+const calculatorValidation = () => {
+  const isEquationValid = (arr, value) => {
+    if (arr.length > 19) {
+      return false;
+    }
+    if (arr.length === 0 && /([/|+|.|×])/.test(value)) {
+      return false;
+    }
+    if (arr[arr.length - 2] === '×' && arr[arr.length - 1] === '×' &&
+      /([/|×|\-|+|.])/.test(value)) {
+      return false;
+    }
+    if (arr[arr.length - 1] === '×' && /([/|\-|+|.])/.test(value)) {
+      return false;
+    }
+    if (/([/|\-|+|.])/.test(arr[arr.length - 1]) &&
+      /([/|\-|+|.|×])/.test(value)) {
+      return false;
+    }
+    if (value === '.') {
+      const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
+      return !lastNumber.includes('.') ? true : false;
+    }
+    if (value === '0' && arr.length > 0) {
+      const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
+      return lastNumber === '0' &&
+      !lastNumber.includes('.') ?
+       false : true;
+    }
+    if (/([1-9])/.test(value) && arr.length > 0) {
+      const lastNumber = arr.join('').split(/[/|\-|+]/).pop();
+      return lastNumber.charAt(0) === '0' && !lastNumber.includes('.') ?
+       false : true;
+    }
+    return true;
+  };
+
+  const isCalculateValid = (arr) => {
+    if (arr.length === 0) {
+      return false;
+    }
+    if (/([/|\-|+|.|×])/.test(arr[arr.length - 1])) {
+      return false;
+    }
+    return true;
+  };
+
+  const isMemoryReadValid = (arr) => {
+    if (arr.length > 19) {
+      return false;
+    }
+    return true;
+  };
+
+  return {isEquationValid, isCalculateValid, isMemoryReadValid};
 };
 
-export default isValid;
+export default calculatorValidation;
