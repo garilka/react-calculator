ead8357b0e13ff720ba9491afc5e5d69431d442c
Ilka <nemmilka@gmail.com>
2022-09-18 15:39:19 +0200

    fix(calculator/server/src/services/memoryService): fix exponential form validation

--- server/src/services/memoryService.js
+++ server/src/services/memoryService.js
@@ -1,13 +1,13 @@
 const memoryService = () => {
   const isMemoryValueValid = (value) => {
+    if (/([0-9]+\.[0-9]+e\+[0-9]+)/.test(value)) {
+      return true;
+    }
     if (typeof value !== 'number') {
       return false;
     }
     if (value.toString().length > 10) {
       return false;
-    }
-    if (/([0-9]+\.[0-9]+e\+[0-9]+)/.test(value)) {
-      return true;
     }
     return true;
   };
