f0858b3556a3f2e06cb9a791734f6d3b9de05148
Ilka <nemmilka@gmail.com>
2022-09-18 17:04:32 +0200

    feat(calculator/server/src/__tests__/memoryService.test.js): test readMemory & writeMemory

--- server/src/__tests__/memoryService.test.js
+++ server/src/__tests__/memoryService.test.js
@@ -1,23 +1,52 @@
-// import {jest} from '@jest/globals';
+import {jest} from '@jest/globals';
+import fs from 'fs';
+import {vol} from 'memfs';
 import memoryService from '../services/memoryService';
 
-const {isMemoryValueValid} = memoryService();
+const {readMemory, writeMemory, isMemoryValueValid} = memoryService();
+
+jest.mock('fs.promises');
+
+describe('readFile() from MemoryService', () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+  it('should return string that\'s saved in the file', async () => {
+    const filePath = '/file.txt';
+    await fs.promises.writeFile(filePath, '20302');
+    const result = await readMemory(filePath, 'utf8');
+    expect(result).toBe('20302');
+  });
+});
+
+describe('writeFile() from MemoryService', () => {
+  beforeEach(() => {
+    vol.reset();
+  });
+  it('should save string into file', async () => {
+    const filePath = '/file.txt';
+    await writeMemory(filePath, '20302', 'utf8');
+    const result = await fs.promises.readFile(filePath, 'utf8');
+    expect(result).toBe('20302');
+  });
+});
 
 describe('isMemoryValueValid() from memoryService', () => {
-  it('returns true on type of number', () => {
+  it('should return true on type of number', () => {
     const valueToSave = 2;
     expect(() => isMemoryValueValid(valueToSave).toBe(true));
   });
-  it('returns false on type of not number', () => {
+  it('should return false on type of not number', () => {
     const valueToSave = '2';
     expect(() => isMemoryValueValid(valueToSave).toBe(false));
   });
-  it('returns false is the length of result greater than 10', () => {
+  it('should return false is the length of result greater than 10', () => {
     const valueToSave = 20302.20302;
     expect(() => isMemoryValueValid(valueToSave).toBe(false));
   });
-  it('returns true on exponential form', () => {
+  it('should return true on exponential form', () => {
     const valueToSave = 2.4580e+70;
     expect(() => isMemoryValueValid(valueToSave).toBe(true));
   });
 });
+
