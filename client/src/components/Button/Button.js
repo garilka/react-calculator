075a84108a9c7c70af8abdbfeb09ef93a807d45f
Ilka <nemmilka@gmail.com>
2022-09-19 21:24:33 +0200

    feat(calculator/client/src/components/Button.js): add style props

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({text, handleClick, height}) => {
+const Button = ({text, handleClick, height, color, fontSize}) => {
   return (
     <div
       className='button'
       onClick={() => handleClick(text)}
-      style={{height: height}}
+      style={{
+        height: height,
+        backgroundColor: color,
+        fontSize: fontSize,
+      }}
     >
       {text}
     </div>);
