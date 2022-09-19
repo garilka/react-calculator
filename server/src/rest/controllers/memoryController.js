69c8e1ef2d0da374cab4dde15e35674419596691
Ilka <nemmilka@gmail.com>
2022-09-19 14:32:17 +0200

    feat(calculator/server/src/rest/controllers/memoryController.js): add more case to put

--- server/src/rest/controllers/memoryController.js
+++ server/src/rest/controllers/memoryController.js
@@ -20,16 +20,27 @@ const get = (req, res) => {
 
 const put = (req, res) => {
   try {
-    const memoryValue = req.body.memoryState === '' ? req.body.result : '';
-    if (isMemoryValueValid(req.body.result)) {
-      writeMemory(filepath, memoryValue.toString(), 'utf8')
-          .then(() => {
-            const message = req.body.memoryState === '' ?
-            `${req.body.result} saved in M` : 'M clear';
-            res.status(200).json({message: message});
-          });
+    if (typeof req.body.memoryState !== 'undefined' &&
+    typeof req.body.result !== 'undefined') {
+      const memoryValue = req.body.memoryState === '' ? req.body.result : '';
+      if (isMemoryValueValid(req.body.result)) {
+        writeMemory(filepath, memoryValue.toString(), 'utf8')
+            .then(() => {
+              if (req.body.memoryState === '') {
+                res.status(200).json({
+                  message: `${req.body.result} saved in M`});
+              } else if (req.body.memoryState === 'M') {
+                res.status(200).json({message: 'M cleared'});
+              } else {
+                res.status(406).json({message: 'Invalid memoryState value'});
+              }
+            });
+      }
+      if (!isMemoryValueValid(req.body.result)) {
+        res.status(406).json({message: 'Invalid value'});
+      }
     } else {
-      res.status(406).json({message: 'Invalid value'});
+      res.status(400).json({message: 'Wrong request body'});
     }
   } catch (err) {
     res.status(500).json({message: err.message});
