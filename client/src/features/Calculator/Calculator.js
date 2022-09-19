d31f7e1232505330cd5efcd59f5a78d6740f3842
Ilka <nemmilka@gmail.com>
2022-09-19 21:29:28 +0200

    feat(calculator/client/src/features/Calculator.js): add color & fontSize props

--- client/src/features/Calculator/Calculator.js
+++ client/src/features/Calculator/Calculator.js
@@ -15,35 +15,42 @@ const Calculator = () => {
         result={result}
         equation={equation}
       />
-      <div className='row'>
-        <Button text='AC' handleClick={clear} height='2em'/>
-        <Button text='DEL' handleClick={deleteLast} height='2em'/>
-        <Button text='MR'handleClick={handleGet} height='2em'/>
-        <Button text='M±'handleClick={handlePut} height='2em'/>
-      </div>
-      <div className='row'>
-        <Button text='7' handleClick={addToEquation}/>
-        <Button text='8' handleClick={addToEquation}/>
-        <Button text='9' handleClick={addToEquation}/>
-        <Button text='/' handleClick={addToEquation}/>
-      </div>
-      <div className='row'>
-        <Button text='4' handleClick={addToEquation}/>
-        <Button text='5' handleClick={addToEquation}/>
-        <Button text='6' handleClick={addToEquation}/>
-        <Button text='×' handleClick={addToEquation}/>
-      </div>
-      <div className='row'>
-        <Button text='1' handleClick={addToEquation}/>
-        <Button text='2' handleClick={addToEquation}/>
-        <Button text='3' handleClick={addToEquation}/>
-        <Button text='-' handleClick={addToEquation}/>
-      </div>
-      <div className='row'>
-        <Button text='0' handleClick={addToEquation}/>
-        <Button text='.' handleClick={addToEquation}/>
-        <Button text='=' handleClick={calculate}/>
-        <Button text='+' handleClick={addToEquation}/>
+      <div className='button-wrapper'>
+        <div className='row'>
+          <Button text='AC' handleClick={clear} height='2em'
+            color='#FFCEAD'/>
+          <Button text='DEL' handleClick={deleteLast} height='2em'
+            color='#FFCEAD'/>
+          <Button text='MR'handleClick={handleGet} height='2em'
+            color='#FFCEAD'/>
+          <Button text='M'handleClick={handlePut} height='2em'
+            color='#FFCEAD'/>
+        </div>
+        <div className='row'>
+          <Button text='7' handleClick={addToEquation}/>
+          <Button text='8' handleClick={addToEquation}/>
+          <Button text='9' handleClick={addToEquation}/>
+          <Button text='/' handleClick={addToEquation} color='#FF8670'/>
+        </div>
+        <div className='row'>
+          <Button text='4' handleClick={addToEquation}/>
+          <Button text='5' handleClick={addToEquation}/>
+          <Button text='6' handleClick={addToEquation}/>
+          <Button text='×' handleClick={addToEquation} color='#FF8670'/>
+        </div>
+        <div className='row'>
+          <Button text='1' handleClick={addToEquation}/>
+          <Button text='2' handleClick={addToEquation}/>
+          <Button text='3' handleClick={addToEquation}/>
+          <Button text='-' handleClick={addToEquation} color='#FF8670'
+            fontSize='2.3vw'/>
+        </div>
+        <div className='row'>
+          <Button text='0' handleClick={addToEquation}/>
+          <Button text='.' handleClick={addToEquation}/>
+          <Button text='=' handleClick={calculate}/>
+          <Button text='+' handleClick={addToEquation} color='#FF8670'/>
+        </div>
       </div>
     </div>
   );
