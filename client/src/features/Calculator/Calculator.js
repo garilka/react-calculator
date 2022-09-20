97f07eeb7f47b86b89bcda839d21e474215e19cf
Ilka <nemmilka@gmail.com>
2022-09-20 11:33:25 +0200

    style(calculator/client/src/features): change style

--- client/src/features/Calculator/Calculator.js
+++ client/src/features/Calculator/Calculator.js
@@ -18,38 +18,41 @@ const Calculator = () => {
       <div className='button-wrapper'>
         <div className='row'>
           <Button text='AC' handleClick={clear} height='2em'
-            color='#FFCEAD'/>
+            color='#83A8EC' textColor='white'/>
           <Button text='DEL' handleClick={deleteLast} height='2em'
-            color='#FFCEAD'/>
+            color='#83A8EC' textColor='white'/>
           <Button text='MR'handleClick={handleGet} height='2em'
-            color='#FFCEAD'/>
+            color='#83A8EC' textColor='white'/>
           <Button text='M'handleClick={handlePut} height='2em'
-            color='#FFCEAD'/>
+            color='#83A8EC' textColor='white'/>
         </div>
         <div className='row'>
           <Button text='7' handleClick={addToEquation}/>
           <Button text='8' handleClick={addToEquation}/>
           <Button text='9' handleClick={addToEquation}/>
-          <Button text='/' handleClick={addToEquation} color='#FF8670'/>
+          <Button text='/' handleClick={addToEquation}
+            color='#F37D68' textColor='white'/>
         </div>
         <div className='row'>
           <Button text='4' handleClick={addToEquation}/>
           <Button text='5' handleClick={addToEquation}/>
           <Button text='6' handleClick={addToEquation}/>
-          <Button text='×' handleClick={addToEquation} color='#FF8670'/>
+          <Button text='×' handleClick={addToEquation}
+            color='#F37D68' textColor='white'/>
         </div>
         <div className='row'>
           <Button text='1' handleClick={addToEquation}/>
           <Button text='2' handleClick={addToEquation}/>
           <Button text='3' handleClick={addToEquation}/>
-          <Button text='-' handleClick={addToEquation} color='#FF8670'
-            fontSize='2.3vw'/>
+          <Button text='-' handleClick={addToEquation}
+            color='#F37D68' textColor='white' fontSize='2.3vw'/>
         </div>
         <div className='row'>
           <Button text='0' handleClick={addToEquation}/>
           <Button text='.' handleClick={addToEquation}/>
           <Button text='=' handleClick={calculate}/>
-          <Button text='+' handleClick={addToEquation} color='#FF8670'/>
+          <Button text='+' handleClick={addToEquation}
+            color='#F37D68' textColor='white'/>
         </div>
       </div>
     </div>
